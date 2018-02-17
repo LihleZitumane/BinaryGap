3eb270143e7cc7a7b6e19c51ec665035cda76a72
Lihle <lihlezytumain@gmail.com>
2018-02-17 23:20:28 +0200

    Task Complete

--- BinaryGap/BinaryGap/Program.cs
+++ BinaryGap/BinaryGap/Program.cs
@@ -8,8 +8,41 @@ namespace BinaryGap
 {
     class Program
     {
+       static public int BinaryGap(int value)
+        {
+            int maxGap = 0;
+            int count = 0;
+            //int value = 1041;
+            string binary = Convert.ToString(value, 2);
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (binary[i] == '1')
+                {
+                    while (i + 1 < binary.Length && binary[i + 1] == '0')
+                    {
+                        count++;
+                        i++;
+                    }
+                }
+                if (i + 1 < binary.Length && binary[i + 1] == '1' && count > maxGap)
+                {
+                    maxGap = count;
+                    count = 0;
+                }
+            }
+            return maxGap;
+        }
+
+
         static void Main(string[] args)
         {
-        }
+           
+            Console.WriteLine(BinaryGap(529));
+            Console.ReadKey();
     }
+
 }
+}
+
+
